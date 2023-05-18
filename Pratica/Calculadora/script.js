9fb30a4ff26f006965c5f7fc67ffea5ef40fc72d
Thiago Luca <102265141+thiago123luca@users.noreply.github.com>
2023-05-18 13:34:15 -0300

    soma

--- Pratica/Calculadora/script.js
+++ Pratica/Calculadora/script.js
@@ -14,9 +14,38 @@ const btnAdd = document.getElementById("btnAdd")
 //     console.log("funcionando")
 //   });
 
+let num1 = ''
+let operador = undefined
+let num2 = ''
+
 function addVisor(value) {
 
     visor.value += value
 
+    if(!operador){
+        num1 += value
+    }else{
+        num2 += value
+    }
+
     console.log("funcionando");
 };
+
+function soma() {
+    
+    console.log(visor.value);
+    
+    visor.value += "+"
+    operador = "+"
+}
+
+function resultado(){
+
+    if(operador = "+"){
+        console.log(Number(num1) + Number(num2))
+        visor.value += `=${Number(num1) + Number(num2)}`
+    }
+    
+
+    console.log(num1, operador, num2)
+}
