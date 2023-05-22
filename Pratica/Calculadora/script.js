c3895f56efc5b89d061938e93d5e61a6a206c1ae
Thiago Luca <102265141+thiago123luca@users.noreply.github.com>
2023-05-22 10:58:46 -0300

    mais-um

--- Pratica/Calculadora/script.js
+++ Pratica/Calculadora/script.js
@@ -1,12 +1,11 @@
 const visor = document.getElementById("visor")
 const historico = document.getElementById("historico") 
 
-const btn1 = document.getElementById("btn1")
-const btn2 = document.getElementById("btn2")
-const btnAdd = document.getElementById("btnAdd")
+
 
 
 let num = ''
+let calc = ''
 
 
 
@@ -15,88 +14,97 @@ function formatarNumero(numero) {
     return numero.toLocaleString('pt-BR');
 }
     
-function formatarNumeroOriginal(numeroFormatado) {
-    var numeroSemPonto = numeroFormatado.replace(/\./g, '');
-    return parseFloat(numeroSemPonto.replace(',', '.'));
-}
+// function formatarNumeroOriginal(numeroFormatado) {
+//     var numeroSemPonto = numeroFormatado.replace(/\./g, '');
+//     return parseFloat(numeroSemPonto.replace(',', '.'));
+// }
   
 
-console.log(formatarNumero(Number(num)))
+// console.log(formatarNumero(Number(num)))
 
 
 function addVisor(value) {
     formatarNumero(Number(value))
      visor.value += value
-     historico.value += value 
+     historico.value += value
+     calc += value
      num += value
-     let resultado = eval(visor.value)
-
 
     console.log("funcionando");
     // console.log('esse é o valor ' + value +' esse é o num '+ num )
 };
 
+console.log(calc)
 
 function expoente(valor) {
-    
-   
+ console.log(calc)
 
     switch(valor){
         case '+':
-            visor.value = eval(visor.value)
-            visor.value += "+";
+            calc = eval(calc)
+            calc += "+"
+            visor.value = calc;
             historico.value += "+"
             num = '';
             break;
         case '-':
-            visor.value = eval(visor.value)
-            visor.value += "-"
+            calc = eval(calc)
+            calc += "-"
+            visor.value = calc;
             historico.value += "-"
             num = ''; 
             break;
         case '*':
-            visor.value = eval(visor.value)
-            visor.value += "*"
+            calc = eval(calc)
+            calc += "*"
+            visor.value = calc;
             historico.value += "*"
             break;
         case '/':
-            visor.value = eval(visor.value)
-            visor.value += "/"
+            calc = eval(calc)
+            calc += "/"
+            visor.value = calc;
             historico.value += "/"
-            break;
-        
-        case '.':
-            num += "."
-            visor.value += "."
-            historico.value += "."
             break;
         default:
             console.log('Algo deu errado')
             break;    
-    }
-    
-    
-    
+    }        
+}
+
+function pontuacao(){
+    calc += "."
+    historico.value += ","
+    visor.value += ","
 }
 
 function resultado(){
-    let resultado = eval(visor.value)
+    let resultado = eval(calc)
+    calc = `${resultado}`
     visor.value = `${resultado}`
     historico.value += `=${resultado} \n${resultado}`
+    console.log(resultado)
     
 }
 
 function limpar(){
+    // historico.value += "\n"
     var linhas = historico.value.split("\n");
     num = ''
     visor.value = ''
+    calc = ''
     if (linhas.length > 1) {
         linhas.pop();
         historico.value = linhas.join("\n");
         historico.value += "\n"
+      }else{
+        historico.value = ""
       }
 }
 
 function apagar(){
-    historico.value
+    console.log(calc)
+    calc = calc.slice(0, -1)
+    historico.value = historico.value.slice(0, -1)
+    visor.value = visor.value.slice(0, -1)
 }
