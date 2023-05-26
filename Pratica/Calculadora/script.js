8d51cc6cd6c9bf56575e1425e231ed0dd0037630
Thiago Luca <102265141+thiago123luca@users.noreply.github.com>
2023-05-26 10:36:47 -0300

    correção

--- Pratica/Calculadora/script.js
+++ Pratica/Calculadora/script.js
@@ -1,52 +1,116 @@
+// Acessar os elementos do HTML
 const visor = document.getElementById("visor")
 const historico = document.getElementById("historico") 
 const calculadora = document.getElementById("calculadora")
 const body = document.getElementById("body")
 const button = document.getElementsByTagName("button")
 
-
 let num = ''
 let calc = ''
 
-
-function calculo(expressao){
-    const operadores = expressao.match(/[+\-*/]/); // Encontrar os operadores na expressão
-    const numeros = expressao.split(/[+\-*/]/).map(parseFloat); // Encontrar os números na expressão e converter para valores numéricos
-    
-    let resultado = numeros[0];
-    
-    if (numeros.length < 2){
-        return resultado
-    }else{
-        for (let i = 0; i < operadores.length; i++) {
-            const operador = operadores[i];
-            const numero = numeros[i + 1];
-        
-            switch (operador) {
-              case '+':
-                resultado += numero;
-                break;
-              case '-':
-                resultado -= numero;
-                break;
-              case '*':
-                resultado *= numero;
-                break;
-              case '/':
-                resultado /= numero;
-                break;
-              default:
-                break;
-            }
-          }
-    }
-  
-    
-  
-    return resultado;
+// adicionar a classe aos botões
+for(let i = 0; button.length > i; i++){
+    button[i].classList.add("buttonUm")
 }
 
 
+// Button troca de cor
+function buttonTrocarCor(){
+
+    // Acessar o padrão de cores do botão
+    const trocaDeCor = document.getElementById("buttonTrocaDeCor")
+    const buttonCor = document.getElementById("buttonCor")
+
+        // verificar em qual padrão de cores está
+        if(trocaDeCor.classList.contains("black")){
+            
+            // troca de cor do corpo da calculadora
+            calculadora.classList.remove("padraoDeCorUm")
+            calculadora.classList.add("padraoDeCorDois");
+
+            // troca de cor do body (fundo)
+            body.style.backgroundColor = 'white'
+           
+            for(let i = 0; button.length > i; i++){
+                button[i].classList.remove("buttonUm")
+                button[i].classList.add("buttonDois")
+            }
+            
+            
+            buttonCor.classList.remove("fas", "fa-sun", "sun-icon")
+            buttonCor.classList.add("fas", "fa-moon", "moon-icon")
+
+            buttonCor.style.color = "#666666"
+
+            trocaDeCor.classList.remove("black")
+            trocaDeCor.classList.add("white")
+
+        }
+        else if (trocaDeCor.classList.contains("white")){
+
+            console.log("desativado");
+            calculadora.classList.remove("padraoDeCorDois");
+            calculadora.classList.add("padraoDeCorUm");
+
+            body.style.backgroundColor = 'black'
+           
+            for(let i = 0; button.length > i; i++){
+                button[i].classList.remove("buttonDois")
+                button[i].classList.add("buttonUm")
+            }
+            
+            buttonCor.classList.remove("fas", "fa-moon", "moon-icon")
+            buttonCor.classList.add("fas", "fa-sun", "sun-icon")
+
+            buttonCor.style.color = "#fdd005"
+
+            trocaDeCor.classList.remove("white")
+            trocaDeCor.classList.add("black")
+        }
+       
+ 
+    
+}
+
+
+
+function calculo(expressao){
+    const operadores = expressao.match(/[+\-*/]/g);
+    const numeros = expressao.split(/[+\-*/]/g).map(parseFloat);
+
+    let resultado = numeros[0];
+
+    if(numeros.length < 2){
+        return resultado
+    }else{
+        for (let i = 0; i < operadores.length; i++) {
+
+            const operador = operadores[i];
+            const numero = numeros[i + 1];
+    
+            switch(operador){
+                case "+":
+                    resultado += numero;
+                    break;
+                case "-":
+                    resultado -= numero;
+                    break;
+                case "*":
+                    resultado *= numero;
+                    break;
+                case "/":
+                    resultado /= numero;
+                    break;
+                default:
+                   break;
+            }
+    
+        }
+    }
+   
+
+    return resultado
+}
 
 
 
@@ -56,13 +120,6 @@ function formatarNumero(numero) {
     return numero.toLocaleString('pt-BR');
 }
     
-// function formatarNumeroOriginal(numeroFormatado) {
-//     var numeroSemPonto = numeroFormatado.replace(/\./g, '');
-//     return parseFloat(numeroSemPonto.replace(',', '.'));
-// }
-  
-
-// console.log(formatarNumero(Number(num)))
 
 
 function addVisor(value) {
@@ -160,60 +217,5 @@ function apagar(){
     visor.value = calc
     num = calc
 }
-for(let i = 0; button.length > i; i++){
-    button[i].classList.add("buttonUm")
-}
 
 
-function trocarCor(){
-
-    const trocaDeCor = document.getElementById("trocaDeCor")
-    const buttonCor = document.getElementById("buttonCor")
-
-        if(trocaDeCor.classList.contains("black")){
-            console.log("ativado");
-            calculadora.classList.remove("div")
-            calculadora.classList.add("div1");
-
-            body.style.backgroundColor = 'white'
-           
-            for(let i = 0; button.length > i; i++){
-                button[i].classList.remove("buttonUm")
-                button[i].classList.add("buttonDois")
-            }
-            
-            
-            buttonCor.classList.remove("fas", "fa-sun", "sun-icon")
-            buttonCor.classList.add("fas", "fa-moon", "moon-icon")
-
-            buttonCor.style.color = "#666666"
-
-            trocaDeCor.classList.remove("black")
-            trocaDeCor.classList.add("white")
-
-        }
-        else if (trocaDeCor.classList.contains("white")){
-
-            console.log("desativado");
-            calculadora.classList.remove("div1");
-            calculadora.classList.add("div");
-
-            body.style.backgroundColor = 'black'
-           
-            for(let i = 0; button.length > i; i++){
-                button[i].classList.remove("buttonDois")
-                button[i].classList.add("buttonUm")
-            }
-            
-            buttonCor.classList.remove("fas", "fa-moon", "moon-icon")
-            buttonCor.classList.add("fas", "fa-sun", "sun-icon")
-
-            buttonCor.style.color = "#fdd005"
-
-            trocaDeCor.classList.remove("white")
-            trocaDeCor.classList.add("black")
-        }
-       
- 
-    
-}
